4eb207a75aba27c13021d1484732414677e8f168
Kosal Vann <kosal.vann@gmail.com>
2018-09-14 06:43:29 -0500

    Added method and tests for overdue status

--- lib/action_items/item.rb
+++ lib/action_items/item.rb
@@ -2,27 +2,44 @@ module ActionItems
     class Item
         attr_accessor :due_date
         attr_accessor :description
-        attr_accessor :completed
+        attr_accessor :completed_date
 
         # the constructor
         #
         # @param [String] due_date The due date for the item
         # @param [String] description The description for the item
         # @param [Boolean] completed If an item is completed or not
-        def initialize(due_date: nil, description: nil, isCompleted: false)
+        def initialize(due_date: nil, description: nil, completed_date: nil)
             @due_date = due_date
             @description = description
-            @completed = isCompleted
+            @completed_date = completed_date
         end
 
-        # Determine the completed status for an action item
+        def pending?
+            completed_date.nil?
+        end
+
+        # Determine wether or not item is completed
         # 
         # @return [Boolean] false Return true if task is completed
-        def isCompleted?(completed)
-            if (completed.length == 0)
-                return false
+        def completed?
+            !pending?
+        end
+
+        def overdue?
+            if completed_date.nil?
+                if due_date # completed date = nil and due_date != nil
+                    return (Time.now > due_date)
+                else # completed date = nil and due_date = nil
+                    return false
+                end
+            else # completed date != nil
+                if completed_date > due_date
+                    return true
+                else
+                    return false
+                end
             end
-            puts completed
         end
     end
 end
--- spec/action_items/item_spec.rb
+++ spec/action_items/item_spec.rb
@@ -3,21 +3,25 @@ require 'action_items/item'
 
 module ActionItems
     describe Item do
-        describe 'constructor' do
+        describe '#initialize' do
             # ensure that the default param for
             # due_date and description are nil
             describe 'with no params' do
 
+                let(:new_item) { Item.new }
+
                 # test when param for description is nil
                 it 'has a nil description' do
-                    new_item = Item.new
                     expect(new_item.description).to be(nil)
                 end
 
                 # test when param for due_date is nil
                 it 'has a nil due date' do
-                    new_item = Item.new
                     expect(new_item.due_date).to be(nil)
+                end
+
+                it 'has a nil completed_date' do
+                    expect(new_item.completed_date).to be_nil
                 end
             end
 
@@ -26,6 +30,7 @@ module ActionItems
             describe 'with params' do
                 let(:description) { 'the best action item ever!' }
                 let(:due_date) { '10/10/2018' }
+                let(:completed_date) { '10/08/2018' }
 
                 # ensure there's a param for description
                 it 'has the passed description' do
@@ -45,15 +50,95 @@ module ActionItems
                     expect(new_item.due_date).to eq(due_date)
                     expect(new_item.description).to eq(description)
                 end
+
+                it 'has the passed completed date' do
+                    new_item = Item.new(completed_date: completed_date)
+                    expect(new_item.completed_date).to eq(completed_date)
+                end
             end
 
             # testing the status completed for an item
-            describe 'Completed' do
-                context 'When testing the completed method' do
-                    # ensure that the default boolean is false
-                    it 'has false as default' do
-                        new_item = Item.new
-                        expect(new_item.completed).to be(false)
+            describe '#completed?' do
+                # ensure that the default boolean is false
+                it 'has false as default' do
+                    new_item = Item.new
+                    expect(new_item.completed?).to be(false)
+                    # expect(new_item).not_to be_completed
+                    # expect(new_item).to_not be_completed
+                end
+            end
+
+            describe '#pending?' do
+                # ensure that the default boolean is true
+                it 'has true as default' do
+                    new_item = Item.new
+                    expect(new_item.pending?).to be(true)
+                    # expect(new_item).not_to be_pending
+                    # expect(new_item).to_not be_pending
+                end
+            end
+
+            describe '#overdue?' do
+
+                before(:all) do
+                    # do some setup only once before all test
+                    # puts "before:all\n"
+                end
+
+                before(:each) do
+                    # before() is same
+                    # do some setup before *each* test
+                    # puts "before:each\n"
+                end
+
+                after(:all) do
+                    # do some setup only once after all test
+                    # puts "after:all\n"
+                end
+
+                after(:each) do
+                    # do some setup after *each* test
+                    # puts "after:each\n"
+                end
+
+                around(:each) do |example|
+                    # do some setup before *each* test
+                    # puts "around:before:each\n"
+                    example.run
+                    # do some setup only after *each* test
+                    # puts "around:after:each\n"
+                end
+                
+                let(:new_item) do
+                    Item.new(due_date: due_date, completed_date: completed_date)
+                end
+
+                context 'when due date is greater than completed date' do
+                    let(:due_date) { '10/10/2018' }
+                    let(:completed_date) { '10/08/2018' }
+
+                    it 'is not overdue' do
+                        # the following line is the same as this:
+                        # expect(new_item.overdue?).to be(false)
+                        expect(new_item).not_to be_overdue
+                    end
+                end
+
+                context 'when due date is less than completed date' do
+                    let(:due_date) { '09/07/2018' }
+                    let(:completed_date) { '10/08/2018' }
+
+                    it 'is overdue' do
+                        expect(new_item).to be_overdue
+                    end
+                end
+
+                context 'when due date is also completed date' do
+                    let(:due_date) { '01/01/2018'}
+                    let(:completed_date) { due_date }
+                    
+                    it 'is not overdue' do
+                        expect(new_item).to_not be_overdue
                     end
                 end
             end
